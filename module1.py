afe2b2fd6d8d09c12fca9830a23755f392c45eeb
Rachana-Kirange <rmkirange29@gmail.com>
2025-06-02 21:00:26 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,6 @@
+a = 10  #decimal
+b = 10.5  #float
+c = 0o123345  #binary
+e = 0b1010    #binary
+d = True      #bool
+f = 0x123abf  #hexadeciaml number
---
+++ module2.py
@@ -0,0 +1,13 @@
+from module1 import a,b,c,d,e,f
+
+print("Type of a = ",type(a),": Value of a = " , a)
+
+print("Type of b = ",type(b),": Value of b = " , b)
+
+print("Type of c = ",type(c),": Value of c = " , c)
+
+print("Type of d = ",type(d),": Value of d = " , d)
+
+print("Type of e = ",type(e),": Value of e = " , e)
+
+print("Type of f = ",type(f),": Value of f = " , f)
